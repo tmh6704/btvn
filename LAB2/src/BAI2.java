77cc9124b6fc89fb68f864ce33cd9b30d3316344
unknown <tranmyhoa709@gmail.com>
2024-01-24 16:57:17 +0700

    BAI 2

---
+++ LAB2/src/BAI2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BAI2 {    public static void main(String[] args) {
+        int a;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("nhap a:");
+        a = sc.nextInt();
+        if(a%2==0)
+        System.out.println("ket qua la so chan");
+        if( a % 2==1)
+        System.out.println("ket qua la so le");
+
+    }
+}
+    
+
