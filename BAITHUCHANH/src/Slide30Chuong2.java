224dd8bd526831081ee17d3f3f446a2d8340e884
unknown <tranmyhoa709@gmail.com>
2024-01-31 17:16:08 +0700

    slide30chuong2

---
+++ BAITHUCHANH/src/Slide30Chuong2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class slide30Chuong2 {
+    public static void main(String[] args) {
+        int n, soDu, tong = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap vao so nguyen duong bat ki: ");
+        n = scanner.nextInt();
+        while (n > 0) {
+            soDu = n % 10;
+            n = n / 10;
+            tong += soDu;
+        }
+        System.out.println("Tong cac chu so" + tong);
+
+        scanner.close();
+    }
+
+}
+
+
+            
+
