aa8a41f8a37486b8cd2c560f6748b71dd1c4efdc
unknown <tranmyhoa709@gmail.com>
2024-02-01 00:19:43 +0700

    slide55chuong2

---
+++ BAITHUCHANH/src/slide55chuong2.java
@@ -0,0 +1,10 @@
+public class slide55chuong2 {
+
+    public static void main(String[] args) {
+        System.out.println("Cac so chia het cho 5 la: ");
+        for(int i = 0; i<=20; i++)
+            if(i % 5 == 0){
+            System.out.println(i);
+            }       
+    }
+}
