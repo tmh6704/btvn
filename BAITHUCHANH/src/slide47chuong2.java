9874a329d3ceea506c6d03fdaaf07eaf47b5b496
unknown <tranmyhoa709@gmail.com>
2024-02-01 00:14:50 +0700

    slide47chuong2

---
+++ BAITHUCHANH/src/slide47chuong2.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class slide47chuong2 {
+    public static void main(String[] args) {
+        int n, sum=0;
+        Scanner sc = new Scanner(System.in);
+        while(sum<=100) {
+            System.out.println("nhap cac so: ");
+            n = sc.nextInt();
+            sum += n;
+            }
+        System.out.print("Tong = " + sum);
+    }
+    
+}
+
