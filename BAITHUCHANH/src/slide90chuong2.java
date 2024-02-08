6ee1963a835e50d0bb4c67dd69cd071d993994a7
unknown <tranmyhoa709@gmail.com>
2024-02-08 10:39:32 +0700

    bai12

--- BAITHUCHANH/src/slide90chuong2.java
+++ BAITHUCHANH/src/slide90chuong2.java
@@ -1,17 +1,20 @@
 import java.util.Scanner;
-public class slide90chuong2{
-   public static void main(String[] args) {
-     String chuoi;
-     char kytu;
-     Scanner sc = new Scanner(System.in);
-     System.out.print("Nhap vao chuoi bat ky: ");
-     chuoi = sc.nextLine();
-     System.out.print("Cac ky tu trong chuoi la: \n");
-      for( int i=0; i<chuoi.length();i++){
-       kytu =chuoi.charAt(i);
-       System.out.println(kytu);
-      }  
-   }
+
+public class slide90chuong2 {
+    public static void main(String[] args) {
+        String chuoi;
+        char kyTu;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("nhap chuoi: ");
+        chuoi = sc.nextLine();
+        System.out.print("Cac ky tu trong chuoi la: ");
+        for (int i=0; i< chuoi.length(); i++){
+            kyTu = chuoi.charAt(i);
+            System.out.print(kyTu);
+        }
+        sc.close();
+    }
+    
 }
     
 
