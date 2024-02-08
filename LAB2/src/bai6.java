42086b1e348a15600fd4f26d8fe6fdabd6deaaa3
unknown <tranmyhoa709@gmail.com>
2024-02-08 10:12:59 +0700

    bai7

--- LAB2/src/bai6.java
+++ LAB2/src/bai6.java
@@ -2,22 +2,17 @@ import java.util.Scanner;
 
 public class bai6 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int n,answer = 1; // Khoi tao gia tri
-
-        do { 
-            System.out.print("Nhap gia tri cho n : ");
-            n = input.nextInt();
-        } while(n < 0);
-
-        System.out.print("Gia tri cua "+n+"! = ");
-        do {
-            if(n == 0 ) continue;
-            else {
-                answer = answer * n;
-                n --;
-            }
-            } while (n > 0);
-        System.out.println(answer);
+        int n, i=1;
+        long  giaithua = 1;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("nhap so: ");
+        n = sc.nextInt();
+        do{   
+            giaithua *=i;
+            i++;
+        }while(i<=n);
+        System.out.print("giai thua cua " + n + " bang " + giaithua  );
+        sc.close();
     }
+    
 }
---
+++ LAB2/src/bai7.java
@@ -0,0 +1,7 @@
+public class bai7 {
+    public static void main(String[] args) {
+        int i;
+        for(i=1; i<=20; i++)
+            System.out.println(i);
+    }
+}
